9f14af0a252c6ede49e966394e787f723dc8372f
NurcahyoHartanto <nurcahyohartanto06@gmail.com>
2023-10-26 18:38:28 +0700

    commit pertama

---
+++ src/Hello1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Hello1 extends Component {
+    render (){
+        return (
+            <div>
+                <h1>Hello Dunia</h1>
+            </div>
+        );
+    }
+}
+
+export default Hello1;
